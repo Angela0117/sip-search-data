9c6c6a680bec2c661dce2a5aef825847dd261431
Angela <angela.huang0117@gmail.com>
2025-05-19 17:16:10 +0800

    測試server.js路由

--- server.js
+++ server.js
@@ -13,27 +13,30 @@ server.use(cors());
 // 應用 json-server 預設中介軟體
 server.use(middlewares)
 //移除 server.db = router.db;：讓 router 自行管理 db.json，避免衝突。
-//server.db = router.db;
+server.db = router.db;
 
 //新加入
-server.use(jsonServer.bodyParser);
+//server.use(jsonServer.bodyParser);
 
-//應用 json-server 路由
-server.use(router)
+
 //應用 json-server-auth（必須在 router 之後）
 server.use(auth);
 
+//應用 json-server 路由
+server.use(router)
+
+
 
 //原先設定
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+// server.listen(3000, () => {
+//   console.log('JSON Server is running')
+// })
 
 //改為使用環境變數 PORT 0519
-// const port = process.env.PORT || 3000;
-// server.listen(port, () => {
-//   console.log(`JSON Server is running on port ${port}`);
-// });
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
 
 // Export the Server API
 module.exports = server
