6a2e5835e06d6be2e65f22c6fb6484ebaf46e1c1
Angela <angela.huang0117@gmail.com>
2025-05-20 00:41:12 +0800

    測試sever.js設定

--- server.js
+++ server.js
@@ -4,19 +4,26 @@ const jsonServer = require('json-server')
 const auth = require("json-server-auth");
 const db = require("./db.json");
 
+// 建立 JSON Server 伺服器
 const server = jsonServer.create()
+
+// 載入資料庫
 const router = jsonServer.router(db);
+
+//預設中介軟體（例如靜態檔案支援、日誌）
 const middlewares = jsonServer.defaults();
 
 // 啟用 CORS
 server.use(cors());
+
+// 允許 JSON POST body
+server.use(jsonServer.bodyParser);
+
 // 應用 json-server 預設中介軟體
 server.use(middlewares)
-//移除 server.db = router.db;：讓 router 自行管理 db.json，避免衝突。
-server.db = router.db;
 
-//新加入
-//server.use(jsonServer.bodyParser);
+//重要！將 router.db 暴露給 auth 使用
+server.db = router.db;
 
 
 //應用 json-server-auth
@@ -32,7 +39,7 @@ server.use(router)
 //   console.log('JSON Server is running')
 // })
 
-//改為使用環境變數 PORT 
+//啟動伺服器，改為使用環境變數 PORT 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
